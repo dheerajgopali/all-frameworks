2edf87a1e4dcd4c2dda7a6e5c6985aeeca01a092
Dheeraj <dheeraj.gopali@gmail.com>
2017-03-26 11:01:40 +0530

    automation and servlets

---
+++ automation/selenium/src/main/java/com/dheeraj/learning/automatoin/selenium/Array.java
@@ -0,0 +1,19 @@
+package com.dheeraj.learning.automatoin.selenium;
+
+/**
+* Created by bhavani on 25-03-2017.
+        */
+public class Array {
+
+    public static void main(String[] args) {
+        int a[]=new int[10];
+        int i;
+        a[1]=10;
+        a[0]=5;
+        a[2]=4;
+        for(i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+
+    }
+}
---
+++ servlets/src/main/java/com/dheeraj/learning/servlets/GreetingServlet.java
@@ -0,0 +1,36 @@
+package com.dheeraj.learning.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GreetingServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		PrintWriter pw =response.getWriter();
+		pw.print("hello");
+		ServletContext context = this.getServletContext();
+		pw.print(this.getServletContext().getRealPath("abc.html"));
+		pw.print(context.getContextPath());
+		pw.println("<br/>"+context.getMimeType("abc"));
+		pw.println("<br/>"+context.getServerInfo());
+		pw.println("<br/>"+context.getServletContextName());
+		pw.println("<br/>"+context.getAttributeNames());
+		pw.println("<br/>"+context.getServletNames());
+		pw.println("<br/>"+request.getCharacterEncoding());
+		pw.println("<br/>"+request.getContentLength());
+		pw.println("<br/>"+request.getContentType());
+		pw.println("<br/>"+request.getInputStream());
+		pw.println("<br/>"+request.getProtocol());
+		pw.println("<br/>"+request.getRemoteAddr());	
+		pw.println("<br/>"+request.getRemoteHost());
+		pw.println("<br/>"+request.getScheme());
+		pw.println("<br/>"+request.getServerName());	
+		pw.println("<br/>"+request.getServerPort());	
+	}
+}
