eb5710671a67be8466e77ef7de8f69f5b28c2ba1
Dheeraj <dheeraj.gopali@gmail.com>
2017-04-24 14:38:58 +0530

    Hibernate sample app. Postgres sample database details

---
+++ orm/hibernate/src/main/java/com/dheeraj/learning/orm/hibernate/ManageActor.java
@@ -0,0 +1,26 @@
+package com.dheeraj.learning.orm.hibernate;
+
+import com.dheeraj.learning.orm.hibernate.domain.Actor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by gopad on 24-04-2017.
+ */
+public class ManageActor {
+    private static SessionFactory factory;
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure()
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Actor a = (Actor)session.get(Actor.class, 1);
+        System.out.println(a);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+}
