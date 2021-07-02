effc54a5e8cff8f2c2d10e1bd39e9ea076d0eab1
Dheeraj <dheeraj.gopali@gmail.com>
2021-07-02 16:21:50 +0530

    Composite completed

---
+++ design-patterns/src/main/java/com/dheeraj/learning/designpatterns/headfirst/composite/Waitress.java
@@ -0,0 +1,11 @@
+package com.dheeraj.learning.designpatterns.headfirst.composite;
+
+public class Waitress {
+    MenuComponent allMenus;
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+    public void printMenu() {
+        allMenus.print();
+    }
+}
